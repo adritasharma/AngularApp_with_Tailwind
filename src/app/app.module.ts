f395e8e6dd567dbe97ae74b7bee6b0e22ee0a3a0
Adrita Sharma <adritasharma.developer@gmail.com>
2021-04-29 13:43:49 +0530

    features modue added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { CoreModule } from 'src/core/core.module';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app.routing.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    CoreModule
+    CoreModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/features/features.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FlexboxComponent } from './flexbox/flexbox.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [
+    FlexboxComponent
+  ]
+})
+export class FeaturesModule { }
